0bfcbce832a4671ec045e3bcd834be31d405de0b
Jofan Fathurahman <fathurahmanjofan@gmail.com>
2024-12-23 12:46:25 +0700

    fix sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -29,6 +29,8 @@
             <span>Dashboard</span>
           </a>
         </li>
+
+        @if (Auth::user()->user_level_id == 'LVL250101001')
         <li>
           <a href="{{route('indexUsers')}}"
             class="menu-item {{ $isActive ? 'text-green-700 bg-[#d9f3ea]' : 'text-gray-800' }} text-sm flex items-center cursor-pointer hover:bg-[#d9f3ea] rounded-md px-3 py-3 transition-all duration-300">
@@ -41,7 +43,6 @@
             <span>Users </span>
           </a>
         </li>
-
         @php
         $activeRoutes = ['indexUsersLevel', 'editUsersLevelById', 'createUsersLevel',];
         $isActive = in_array(Request::route()->getName(), $activeRoutes);
@@ -58,6 +59,9 @@
             <span>Users Level</span>
           </a>
         </li>
+        @endif
+
+        @if (Auth::user()->user_level_id == 'LVL250101001'|| Auth::user()->user_level_id == 'LVL250101002')
         @php
         $activeRoutes = ['indexOlt','allSite','siteIndex', 'createOlt', 'editOltById', 'indexOdp', 'createOdp',
         'editOdpById'];
@@ -116,26 +120,11 @@
           </ul>
         </li>
 
+
         @php
         $activeRoutes = [ 'coverage'];
         $isActive = in_array(Request::route()->getName(), $activeRoutes);
         @endphp
-        <li>
-          <a href="{{route('coverage')}}"
-            class="menu-item text-gray-800 text-sm 
-            {{ $isActive ? 'text-green-700 bg-[#d9f3ea]' : 'text-gray-800' }} flex items-center cursor-pointer hover:bg-[#d9f3ea] rounded-md px-3 py-3 transition-all duration-300">
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" class="mr-4" viewBox="0 0 24 24"
-              style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
-              <path
-                d="m12 17 1-2V9.858c1.721-.447 3-2 3-3.858 0-2.206-1.794-4-4-4S8 3.794 8 6c0 1.858 1.279 3.411 3 3.858V15l1 2zM10 6c0-1.103.897-2 2-2s2 .897 2 2-.897 2-2 2-2-.897-2-2z">
-              </path>
-              <path
-                d="m16.267 10.563-.533 1.928C18.325 13.207 20 14.584 20 16c0 1.892-3.285 4-8 4s-8-2.108-8-4c0-1.416 1.675-2.793 4.267-3.51l-.533-1.928C4.197 11.54 2 13.623 2 16c0 3.364 4.393 6 10 6s10-2.636 10-6c0-2.377-2.197-4.46-5.733-5.437z">
-              </path>
-            </svg>
-            <span>Coverage</span>
-          </a>
-        </li>
         <li>
           @php
           $activeRoutes = [ 'site'];
@@ -150,6 +139,24 @@
               </path>
             </svg>
             <span>Site</span>
+          </a>
+        </li>
+        @endif
+
+        <li>
+          <a href="{{route('coverage')}}"
+            class="menu-item text-gray-800 text-sm 
+                        {{ $isActive ? 'text-green-700 bg-[#d9f3ea]' : 'text-gray-800' }} flex items-center cursor-pointer hover:bg-[#d9f3ea] rounded-md px-3 py-3 transition-all duration-300">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" class="mr-4" viewBox="0 0 24 24"
+              style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
+              <path
+                d="m12 17 1-2V9.858c1.721-.447 3-2 3-3.858 0-2.206-1.794-4-4-4S8 3.794 8 6c0 1.858 1.279 3.411 3 3.858V15l1 2zM10 6c0-1.103.897-2 2-2s2 .897 2 2-.897 2-2 2-2-.897-2-2z">
+              </path>
+              <path
+                d="m16.267 10.563-.533 1.928C18.325 13.207 20 14.584 20 16c0 1.892-3.285 4-8 4s-8-2.108-8-4c0-1.416 1.675-2.793 4.267-3.51l-.533-1.928C4.197 11.54 2 13.623 2 16c0 3.364 4.393 6 10 6s10-2.636 10-6c0-2.377-2.197-4.46-5.733-5.437z">
+              </path>
+            </svg>
+            <span>Coverage</span>
           </a>
         </li>
       </ul>

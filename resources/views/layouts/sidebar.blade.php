4e2fd447691e7f7fb1bff94214d302306211ff8f
jofan-cah <jofan.fathurahman@gmail.com>
2025-01-03 15:03:25 +0700

    fixLoading

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,8 +2,9 @@
   <div id="sidebar-collapse-menu"
     class=" bg-white shadow-lg h-screen fixed top-0 left-0 overflow-auto z-[99] lg:min-w-[250px] lg:w-max max-lg:w-0 max-lg:invisible transition-all duration-500">
     <div class="pt-8 pb-2 px-6 sticky top-0 bg-slate-100 min-h-[80px] z-[100]">
-      <a href="javascript:void(0)" class="outline-none"><img
-          src="{{ asset('fiberOne.png') }}" alt="logo"
+      <a href="javascript:void(0)" class="outline-none">
+        <img
+          src="{{ asset('fiberOne.png') }}" loading="lazy"  alt="logo"
           class='w-[170px]' />
       </a>
     </div>
